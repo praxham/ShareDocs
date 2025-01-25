2defb34be7293213a4fcfc17beba896c83bc44d1
prathamnaayatrade <83165113+praxham@users.noreply.github.com>
2025-01-25 17:16:58 +0530

    firebase integration commented

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,23 @@ import App from './App.jsx'
 import './index.css'
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY || process.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN || process.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID || process.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET || process.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || process.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APPID || process.env.VITE_FIREBASE_APPID
-};
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY || process.env.VITE_FIREBASE_APIKEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN || process.env.VITE_FIREBASE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECTID || process.env.VITE_FIREBASE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET || process.env.VITE_FIREBASE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || process.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APPID || process.env.VITE_FIREBASE_APPID
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
